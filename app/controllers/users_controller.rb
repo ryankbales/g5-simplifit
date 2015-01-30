f9df282797d37a86bcfa42dcf9a4530fe7b076fd
Ryan Bales <ryankbales@gmail.com>
2015-01-29 20:07:43 -0800

    routes and models intial setup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+	def new
+    @user = User.new
+  end
+
+  def show
+
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "You are registered!"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your Profile Was Updated"
+      redirect_to user_path
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :age, :gender, :department, :timezone)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # def require_same_user
+  #   if current_user != @user
+  #     flash[:error] = "This ain't your deal dude!"
+  #     redirect_to root_path
+  #   end
+  # end
+end
