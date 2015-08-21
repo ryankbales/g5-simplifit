3c71f4f3bb4f0c64eac3d845788f52eed484b0dd
Ryan Bales <ryankbales@gmail.com>
2015-08-21 14:27:47 -0700

    fixing specs

--- spec/controllers/workouts_controller_spec.rb
+++ spec/controllers/workouts_controller_spec.rb
@@ -1,25 +1,18 @@
 require 'rails_helper'
 
 describe WorkoutsController do
-  
-  context "GET :new for authenticated user without a profile", :auth_controller do
-    it "redirects to sign in path" do
-      get :new
-      expect(response).to redirect_to new_simplifit_user_path
-    end
-  end
 
   describe "POST #create" do
     context "with a user that is authenticated and has a profile", :auth_controller do
       let(:ryan) { Fabricate(:simplifit_user, user_email: user.email) }
       let(:category) { Fabricate(:workout_category) }
       it "should redirect to user path" do
-        post :create, workout: Fabricate.attributes_for(:workout, simplifit_user_id: ryan.id, workout_category_id: category.id, duration: 60)
+        post :create, simplifit_user_id: ryan.id, workout: Fabricate.attributes_for(:workout, simplifit_user_id: ryan.id, workout_category_id: category.id, duration: 60)
         expect(response).to redirect_to simplifit_user_path(ryan)
       end
 
       it "should set the notice" do
-        post :create, workout: Fabricate.attributes_for(:workout, simplifit_user_id: ryan.id, workout_category_id: category.id, duration: 60)
+        post :create, simplifit_user_id: ryan.id, workout: Fabricate.attributes_for(:workout, simplifit_user_id: ryan.id, workout_category_id: category.id, duration: 60)
         expect(flash[:success]).to be_present
       end
     end
@@ -28,15 +21,15 @@ describe WorkoutsController do
       let(:ryan) { Fabricate(:simplifit_user, user_email: user.email) }
       let(:category) { Fabricate(:workout_category) }
       it "should redirect to user path" do
-        post :create, workout: Fabricate.attributes_for(:workout, simplifit_user_id: ryan.id, workout_category_id: category.id)
+        post :create, simplifit_user_id: ryan.id, workout: Fabricate.attributes_for(:workout, simplifit_user_id: ryan.id, workout_category_id: category.id)
         expect(response).to redirect_to simplifit_user_path(ryan)
       end
       it "should not create a workout" do
-        post :create, workout: Fabricate.attributes_for(:workout, simplifit_user_id: ryan.id, workout_category_id: category.id)
+        post :create, simplifit_user_id: ryan.id, workout: Fabricate.attributes_for(:workout, simplifit_user_id: ryan.id, workout_category_id: category.id)
         expect(Workout.count).to eq(0)
       end
       it "should set error notice" do
-        post :create, workout: Fabricate.attributes_for(:workout, simplifit_user_id: ryan.id, workout_category_id: category.id)
+        post :create, simplifit_user_id: ryan.id, workout: Fabricate.attributes_for(:workout, simplifit_user_id: ryan.id, workout_category_id: category.id)
         expect(flash[:error]).to be_present
       end
     end
